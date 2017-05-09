9a37aa29f80dfc4f184e31a47b88e5b8fadffd13
Rafaela <rafaelaga46@gmail.com>
2017-05-09 13:30:58 -0300

    etapa 4

--- ETAPA4.cs
+++ ETAPA4.cs
@@ -7,62 +7,54 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        struct carro
+        static void Main(string[] args)
         {
-            public int p;
-            public int quantidade;
-            public string nome;
-            public double km;
+            int i, quantidade, p;
+            string nome;
+            double km;
 
-            static void Main(string[] args)
+            Console.WriteLine("Informe a quantidade de carros");
+            quantidade = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 0; i < qtd; i++)
             {
-                carro auto;
-                int i;
+                Console.WriteLine("Informe o nome do carro");
+                nome = Console.ReadLine();
 
-                Console.WriteLine("Informe a quantidade de carros");
-                auto.quantidade = Convert.ToInt32(Console.ReadLine());
-                
-                for (i = 0; i < auto.quantidade; i++)
-                {
-                    Console.WriteLine("Informe o nome do carro");
-                    auto.nome = Console.ReadLine();
+                Console.WriteLine("Informe a quilometragem do carro");
+                km = double.Parse(Console.ReadLine());
 
-                    Console.WriteLine("Informe a quilometragem do carro");
-                    auto.km = double.Parse(Console.ReadLine());
+                Console.WriteLine("Informe a potencia do carro");
+                p = int.Parse(Console.ReadLine());
 
-                    Console.WriteLine("Informe a potencia do carro");
-                    auto.p = int.Parse(Console.ReadLine());
-
-                    Console.WriteLine(Classificar(auto.nome, auto.km, auto.p));
-
-                }
-            } //static void main
-
-            public static string Classificar(string modelo, double km, int potencia)
-            {
-                string T, P;
-
-                if (km <= 5000)
-                    T = "novo";
-
-                else if (km <= 30000)
-                    T = "seminovo";
-
-                else
-                    T = "velho";
-
-                if (potencia < 120)
-                    P = "popular";
-
-                else if (potencia <= 200)
-                    P = "forte";
-
-                else
-                    P = "potente";
-
-                return String.Format("{0} - {1} - {2}", modelo, T, P);
+                Console.WriteLine(Classificar(nome, km, p));
 
             }
+        } //static void main
+
+        public static string Classificar(string modelo, double km, int pot)
+        {
+            string T, P;
+
+            if (km <= 5000)
+                T = "novo";
+
+            else if (km <= 30000)
+                T = "seminovo";
+
+            else
+                T = "velho";
+
+            if (pot < 120)
+                P = "popular";
+
+            else if (pot <= 200)
+                P = "forte";
+
+            else
+                P = "potente";
+
+            return String.Format("{0} - {1} - {2}", modelo, T, P);
         }
     }
 }
