921bbdbd3b90a8b8d00ed16fa9a8ee4f4be5102a
rafa <rafaelaga46@gmail.com>
2017-06-06 16:06:12 -0300

    atualizae

---
+++ Soluções/Competidor/Competidor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Competidor
+    { 
+        public string nome;
+        public double t1;
+        public double t2;
+        public double t3;
+
+        public double tempoOficial()
+        {
+            if (t1 > t2 && t1 > t3)
+                return t2 + t3;
+            else if (t2 > t1 && t2 > t3)
+                return t1 + t3;
+            else if (t3 > t2 && t3 > t1)
+                return t2 + t1;
+        }
+    }
+}
