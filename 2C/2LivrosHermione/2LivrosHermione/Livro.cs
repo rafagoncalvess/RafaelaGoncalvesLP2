30b5c219ff39c9178710a832607012d13dd1ce86
Danilo Santos <danilovid@yahoo.com.br>
2017-07-11 15:24:33 -0300

    Atualização e organização

---
+++ 2C/2LivrosHermione/2LivrosHermione/Livro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2LivrosHermione
+{
+    class Livro
+    {
+        public string titulo;
+        public int qtdpag, DiasAteDev;
+
+        public int TempoDeLeitura(int paghr)
+        {
+            return qtdpag / paghr;
+        }
+    }
+}
