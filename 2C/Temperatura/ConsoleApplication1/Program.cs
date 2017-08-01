5e0bcd76b19df858c49296e3b4bb1a3da56cfa2c
Rafaela <rafaelaga46@gmail.com>
2017-08-01 15:59:34 -0300

    atualizado

---
+++ 2C/Temperatura/ConsoleApplication1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TempC t1 = new TempC();
+            t1.valor = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite 1 para Farenheit, 2 para Kelvin, 3 para nova temperatura, 0 para sair");
+            int comando = int.Parse(Console.ReadLine());
+
+            while (comando != 0)
+            {
+                if (comando == 1)
+                {
+                    Console.WriteLine(t1.EmFarenheit());
+                }
+                else if (comando == 2)
+                {
+                    Console.WriteLine(t1.EmKelvin());
+                }
+                else if (comando == 3)
+                {
+                    t1.valor = Convert.ToDouble(Console.ReadLine());
+                }
+                else
+                    Console.WriteLine("comando invalido");
+
+                Console.WriteLine("Digite 1 para Farenheit, 2 para Kelvin, 3 para nova temperatura, 0 para sair");
+                comando = int.Parse(Console.ReadLine());
+            }
+           
+
+
+            Console.WriteLine(t1.EmFarenheit());
+            Console.WriteLine(t1.EmKelvin());
+
+
+
+            
+
+        }
+    }
+}
---
+++ 2C/Temperatura/ConsoleApplication1/TempC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class TempC
+    {
+        public double valor;
+
+        public double EmFarenheit()
+        {
+            double f = 1.8 * valor + 32;
+            return f;
+        }
+        public double EmKelvin()
+        {
+            double k = valor + 273;
+            return k;
+        }
+    }
+}
