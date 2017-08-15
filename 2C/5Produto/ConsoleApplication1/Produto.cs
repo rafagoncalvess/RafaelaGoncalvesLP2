eeeb2aedeaecf1e1a47e916fd6c61cd44e04a9e2
Rafaela <rafaelaga46@gmail.com>
2017-08-15 15:50:09 -0300

    Atualizar

---
+++ 2C/5Produto/ConsoleApplication1/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Produto
+    {
+        private int id;
+
+        public int Id {
+            get {return id;}
+        }
+        public string Nome { get; set; }
+
+        private int quatidade; 
+        public int Quantidade { 
+            get {return quatidade;}
+        }
+        public double Preco { get; set; }
+
+        public void Repor(int repor)
+        {
+            this.quatidade += repor;
+        }    
+
+        public Produto(int id, string nome, int preco)
+        {
+            this.id = id;
+            this.Nome = nome;
+            this.Preco = preco;
+            this.quatidade = 0;
+        }
+    }
+}
