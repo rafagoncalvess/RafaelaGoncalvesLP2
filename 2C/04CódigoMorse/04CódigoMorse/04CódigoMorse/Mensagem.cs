309f8dfc01eda44900ad7967299c9f5707f98b05
Rafaela <rafaelaga46@gmail.com>
2017-08-15 15:52:57 -0300

    ATUALIZAR

---
+++ 2C/04CódigoMorse/04CódigoMorse/04CódigoMorse/Mensagem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04CódigoMorse
+{
+    class Mensagem
+    {
+        private string[] tabela = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+
+        public string Texto { get; set; }
+
+        public string Codificar()
+        {
+
+        }
+
+        public string Decodificar()
+        {
+
+        }
+
+        public void Transmitir()
+        {
+
+        }
+    }
+}
---
+++ 2C/04CódigoMorse/04CódigoMorse/04CódigoMorse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04CódigoMorse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 - Condificar. \n2 - Decodificar");
+
+            int op = int.Parse(Console.ReadLine());
+            Mensagem m = new Mensagem();
+            m.Texto = Console.ReadLine();
+
+            if (op == 1)
+                Console.WriteLine(m.Codificar());
+            else if (op == 2)
+                Console.WriteLine(m.Decodificar());
+            else
+                Console.WriteLine("Inválido");
+
+        }
+    }
+}
