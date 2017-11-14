4ae317d19b6fc61c718e09b3306332a8439e089c
rafaela <rafaelaga46@gmail.com>
2017-11-14 17:37:06 -0300

    atualizado

---
+++ Rascunhos/PrimeiroBank/PrimeiroBank/Carro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroBank
+{
+    class Carro
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Modelo { get; set; }
+        public string Cor { get; set; }
+        public string Placa { get; set; }
+        public string Marca { get; set; }
+        public int Potencia { get; set; }
+
+
+        public string Registro()
+        {
+            return string.Format(@"INSERT 
+                                 INTO CARRO(marca,modelo,cor,placa,potencia)
+                                 VALUES ('{0}', '{1}', '{2}', '{3}', {4});", @Marca, @Modelo, @Cor, @Placa, @Potencia);
+        }
+    }
+}
---
+++ Rascunhos/PrimeiroBank/PrimeiroBank/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace PrimeiroBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c = new Carro();
+
+            Console.WriteLine("insira modelo");
+            c.Modelo = Console.ReadLine();
+            Console.WriteLine("insira cor");
+            c.Cor = Console.ReadLine();
+            Console.WriteLine("insira placa");
+            c.Placa = Console.ReadLine();
+            Console.WriteLine("insira marca");
+            c.Marca = Console.ReadLine();
+            Console.WriteLine("insira potencia");
+            c.Potencia = Convert.ToInt32(Console.ReadLine());
+
+            
+            SqlConnection conex = new SqlConnection("Data Source = localhost; Initial Catalog = AulaLp; Integrated Security = SSPI;");
+            SqlCommand comd = new SqlCommand();
+
+            comd.Connection = conex;
+            comd.CommandText = c.Registro();
+            comd.Parameters.AddWithValue("Marca", c.Marca);
+            comd.Parameters.AddWithValue("Modelo", c.Modelo);
+            comd.Parameters.AddWithValue("Cor", c.Cor);
+            comd.Parameters.AddWithValue("Placa", c.Placa);
+            comd.Parameters.AddWithValue("Potencia", c.Potencia);
+
+
+            conex.Open();
+            comd.ExecuteNonQuery();
+            conex.Close();
+
+
+
+            Console.WriteLine("OK");
+        }
+    }
+}
